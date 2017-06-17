66e125f45285c0eb6916757a731a6bbde713dafe
Antoine <antoine.savalle1@viacesi.fr>
2017-06-17 19:36:04 +0200

    Update and rename scripMap.sql to scripADD.sql

---
+++ scripADD.sql
@@ -0,0 +1,22 @@
+
+
+INSERT INTO TYPEblock (Name_Block, Folder_block) VALUES
+('diamond', 'res/sprites/textures.png'),
+('rock', 'res/sprites/textures.png'),
+('mud', 'res/sprites/textures.png'),
+('wall', 'res/sprites/textures.png'),
+('void', 'res/sprites/textures.png');
+
+
+INSERT INTO TYPEentity (Name_entity, Folder_entity, DropType) VALUES
+('Human', 'res/sprites/human.png', ''),
+('Buzzbuzz', 'res/sprites/textures.png', 'points'),
+('Pingping', 'res/sprites/textures.png', 'points'),
+('Flapflap', 'res/sprites/textures.png', 'points'),
+('Bubblebubble', 'res/sprites/textures.png', 'points'),
+('Bimbim', 'res/sprites/textures.png', 'points'),
+('Guriguri', 'res/sprites/textures.png', 'points'),
+('Puffpuff', 'res/sprites/textures.png', 'diamond'),
+('Piropiro', 'res/sprites/textures.png', 'diamond'),
+('TackyTacky', 'res/sprites/textures.png', 'diamond'),
+('Amoeba', 'res/sprites/textures.png', 'diamond');

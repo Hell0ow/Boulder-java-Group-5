89c1ed47c8e337bbaf3a80ed97653a7bcc47391c
Antoine <antoine.savalle1@viacesi.fr>
2017-06-19 20:10:11 +0200

    Update scripADD.sql

--- scripADD.sql
+++ scripADD.sql
@@ -1,22 +1,18 @@
 
 
 INSERT INTO TYPEblock (Name_Block, Folder_block) VALUES
-('diamond', 'res/sprites/textures.png'),
-('rock', 'res/sprites/textures.png'),
-('mud', 'res/sprites/textures.png'),
-('wall', 'res/sprites/textures.png'),
-('void', 'res/sprites/textures.png');
+('Diamond', 'res/sprites/textures.png'),
+('Rock', 'res/sprites/textures.png'),
+('Mud', 'res/sprites/textures.png'),
+('Wall', 'res/sprites/textures.png'),
+('Air', 'res/sprites/textures.png');
 
 
 INSERT INTO TYPEentity (Name_entity, Folder_entity, DropType) VALUES
 ('Human', 'res/sprites/human.png', ''),
 ('Buzzbuzz', 'res/sprites/textures.png', 'points'),
-('Pingping', 'res/sprites/textures.png', 'points'),
 ('Flapflap', 'res/sprites/textures.png', 'points'),
-('Bubblebubble', 'res/sprites/textures.png', 'points'),
-('Bimbim', 'res/sprites/textures.png', 'points'),
 ('Guriguri', 'res/sprites/textures.png', 'points'),
-('Puffpuff', 'res/sprites/textures.png', 'diamond'),
 ('Piropiro', 'res/sprites/textures.png', 'diamond'),
 ('TackyTacky', 'res/sprites/textures.png', 'diamond'),
 ('Amoeba', 'res/sprites/textures.png', 'diamond');

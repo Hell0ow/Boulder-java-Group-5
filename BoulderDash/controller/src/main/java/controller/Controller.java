ebea4a17ee5080d84f1d158a698021b8d99e8352
Hell0ow <antoine.savalle1@viacesi.fr>
2017-06-21 16:42:39 +0200

    Revert "UPDATE Controller"
    This reverts commit 92d3db3cb038cda791c9758e1c7e0192683473b8.

--- BoulderDash/controller/src/main/java/controller/Controller.java
+++ BoulderDash/controller/src/main/java/controller/Controller.java
@@ -1,11 +1,8 @@
 package controller;
 
-import java.util.Scanner;
-
 import Icontroller.IController;
 import Imodel.IModel;
 import Iview.IView;
-import model.Order;
 
 public class Controller implements IController{
 
@@ -56,47 +53,5 @@ public class Controller implements IController{
 		database.closeDatabase();
 		
 		
-		 try {
-	            while(true) {
-
-	                view.frame();
-
-	                model.cycle(getOrder());
-	            }
-	        } catch (Exception e) {
-	            System.out.println(e.getMessage());
-	            e.printStackTrace();
-	        }
-		
-		
 	}
-	
-	public static Order getOrder() {
-        Scanner sc = new Scanner(System.in);
-
-        sc.hasNextLine();
-
-        String string = sc.nextLine();
-
-        switch(string) {
-            case "MOVE_UP":
-                return Order.MOVE_UP;
-            case "MOVE_RIGHT":
-                return Order.MOVE_RIGHT;
-            case "MOVE_DOWN":
-                return Order.MOVE_DOWN;
-            case "MOVE_LEFT":
-                return Order.MOVE_LEFT;
-            case "DIG_UP":
-                return Order.DIG_UP;
-            case "DIG_RIGHT":
-                return Order.DIG_RIGHT;
-            case "DIG_DOWN":
-                return Order.DIG_DOWN;
-            case "DIG_LEFT":
-                return Order.DIG_LEFT;
-            default:
-                return Order.NONE;
-        }
-    }
 }

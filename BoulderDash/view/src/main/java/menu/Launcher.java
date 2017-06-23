a316b6a8fc98a223cdec135a18953cb6c3bfe456
NicolasV-h <nicolas.vanhove@viacesi.fr>
2017-06-23 11:28:43 +0200

    Update Launcher.java

--- BoulderDash/view/src/main/java/menu/Launcher.java
+++ BoulderDash/view/src/main/java/menu/Launcher.java
@@ -6,10 +6,10 @@ import menu.Game;
  * Created by Nicolas on 21/06/2017.
  */
 public class Launcher {
-	
-    public static void runMenu(){
+    public static void main(String[] args){
          Game game = new Game("Boulder Dash", 1152,832);
          game.start();
-         Sound.music("D:/CESI année 1/Projet/Java/Sound/Boulder_Dash_Theme.wav", 100000, 3);
+         new sound(100000,1);
+
     }
 }

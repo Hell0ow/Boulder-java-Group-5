fccc2e43d2f1936596b396e294f261c2ff8cd455
NicolasV-h <nicolas.vanhove@viacesi.fr>
2017-06-23 16:20:28 +0200

    Update Assets.java

--- BoulderDash/view/src/main/java/gfx/Assets.java
+++ BoulderDash/view/src/main/java/gfx/Assets.java
@@ -5,20 +5,33 @@ import java.awt.image.BufferedImage;
 
 /**
  * Created by Nicolas on 21/06/2017.
+ * load all the images
  */
 public class Assets {
-
-    public static BufferedImage background, titleAnimation;
+/**
+     * The constant background.
+     */
+    public static BufferedImage background,
+    /**
+     * The Title animation.
+     */
+    titleAnimation;
+    /**
+     * The image for Display the fps number and set the music's volume
+     */
     public static BufferedImage speaker_muted, speaker_low, speaker_high, canSee, cantSee;
     public static Rectangle playButton, settingsButton;
+    /**
+    * Init. load image
+    */
 
     public static void init(){
-        background = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/backgroun.png");
-        titleAnimation = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/Title_Boulder.png");
-        speaker_muted = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/no_volume.png");
-        speaker_low = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/minus_volume.png");
-        speaker_high = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/plus_volume.png");
-        canSee = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/yeux.png");
-        cantSee = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/yeux_barre.png");
+        background = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/backgroun.png");
+        titleAnimation = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/Title_Boulder.png");
+        speaker_muted = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/no_volume.png");
+        speaker_low = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/minus_volume.png");
+        speaker_high = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/plus_volume.png");
+        canSee = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/yeux.png");
+        cantSee = ImageLoader.loadImage("D:/CESI annÃ©e 1/Projet/Java/Sprites/yeux_barre.png");
     }
 }

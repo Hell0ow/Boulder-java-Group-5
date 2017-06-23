44f2a16263db2fd30c1aaf48498bc9309f44137a
Hell0ow <antoine.savalle1@viacesi.fr>
2017-06-23 17:43:20 +0200

    Revert "Update scriptGeneration.sql"
    This reverts commit ab032de66d3f17db1dad302eeb031571c4e4f1f6.

--- scriptGeneration.sql
+++ scriptGeneration.sql
@@ -8,14 +8,12 @@
 #------------------------------------------------------------
 
 CREATE TABLE MAP(
-        ID_map    int (11) Auto_increment  NOT NULL ,
-        Name_map  Text NOT NULL ,
-        Xmin_map  Int NOT NULL ,
-        Xmax_map  Int NOT NULL ,
-        Ymax_map  Int NOT NULL ,
-        Ymin_map  Int NOT NULL ,
-        nbDiamond Int ,
-        mapTime   Int ,
+        ID_map   int (11) Auto_increment  NOT NULL ,
+        Name_map Text NOT NULL ,
+        Xmin_map Int NOT NULL ,
+        Xmax_map Int NOT NULL ,
+        Ymax_map Int NOT NULL ,
+        Ymin_map Int NOT NULL ,
         PRIMARY KEY (ID_map )
 )ENGINE=InnoDB;
 
@@ -26,10 +24,8 @@ CREATE TABLE MAP(
 
 CREATE TABLE BLOCK(
         ID_Block     int (11) Auto_increment  NOT NULL ,
-        X_block      Int NOT NULL ,
-        Y_block      Int NOT NULL ,
-        ID_map       Int NOT NULL ,
-        ID_typeBlock Int ,
+        Name_block   Text NOT NULL ,
+        Folder_block Text NOT NULL ,
         PRIMARY KEY (ID_Block )
 )ENGINE=InnoDB;
 
@@ -46,41 +42,15 @@ CREATE TABLE PLAYER(
 
 
 #------------------------------------------------------------
-# Table: ENTITY
+# Table: compose
 #------------------------------------------------------------
 
-CREATE TABLE ENTITY(
-        ID_Entity     int (11) Auto_increment  NOT NULL ,
-        X_entity      Int NOT NULL ,
-        Y_entity      Int NOT NULL ,
-        ID_map        Int NOT NULL ,
-        ID_typeEntity Int NOT NULL ,
-        PRIMARY KEY (ID_Entity )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: TYPEblock
-#------------------------------------------------------------
-
-CREATE TABLE TYPEblock(
-        ID_typeBlock int (11) Auto_increment  NOT NULL ,
-        Name_block   Text NOT NULL ,
-        Folder_block Text NOT NULL ,
-        PRIMARY KEY (ID_typeBlock )
-)ENGINE=InnoDB;
-
-
-#------------------------------------------------------------
-# Table: TYPEentity
-#------------------------------------------------------------
-
-CREATE TABLE TYPEentity(
-        ID_typeEntity int (11) Auto_increment  NOT NULL ,
-        Name_entity   Text NOT NULL ,
-        Folder_entity Text NOT NULL ,
-        DropType Text ,
-        PRIMARY KEY (ID_typeEntity )
+CREATE TABLE compose(
+        X_comp   Int NOT NULL ,
+        Y_comp   Int NOT NULL ,
+        ID_map   Int NOT NULL ,
+        ID_Block Int NOT NULL ,
+        PRIMARY KEY (ID_map ,ID_Block )
 )ENGINE=InnoDB;
 
 
@@ -95,9 +65,7 @@ CREATE TABLE play(
         PRIMARY KEY (ID_map ,ID_player )
 )ENGINE=InnoDB;
 
-ALTER TABLE BLOCK ADD CONSTRAINT FK_BLOCK_ID_map FOREIGN KEY (ID_map) REFERENCES MAP(ID_map);
-ALTER TABLE BLOCK ADD CONSTRAINT FK_BLOCK_ID_typeBlock FOREIGN KEY (ID_typeBlock) REFERENCES TYPEblock(ID_typeBlock);
-ALTER TABLE ENTITY ADD CONSTRAINT FK_ENTITY_ID_map FOREIGN KEY (ID_map) REFERENCES MAP(ID_map);
-ALTER TABLE ENTITY ADD CONSTRAINT FK_ENTITY_ID_typeEntity FOREIGN KEY (ID_typeEntity) REFERENCES TYPEentity(ID_typeEntity);
+ALTER TABLE compose ADD CONSTRAINT FK_compose_ID_map FOREIGN KEY (ID_map) REFERENCES MAP(ID_map);
+ALTER TABLE compose ADD CONSTRAINT FK_compose_ID_Block FOREIGN KEY (ID_Block) REFERENCES BLOCK(ID_Block);
 ALTER TABLE play ADD CONSTRAINT FK_play_ID_map FOREIGN KEY (ID_map) REFERENCES MAP(ID_map);
 ALTER TABLE play ADD CONSTRAINT FK_play_ID_player FOREIGN KEY (ID_player) REFERENCES PLAYER(ID_player);

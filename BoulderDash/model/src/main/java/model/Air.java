1c4714ca0b76b8bbb78667752c5582fc8c25d980
Hell0ow <antoine.savalle1@viacesi.fr>
2017-06-23 18:05:13 +0200

    CORRECTION pull request

--- BoulderDash/model/src/main/java/model/Air.java
+++ BoulderDash/model/src/main/java/model/Air.java
@@ -5,7 +5,7 @@ import Imodel.IAir;
 public class Air extends Block implements IAir {
 	
 	public Air() throws Exception {
-		super("Air", "D:/CESI annÃ©e 1/Projet/Java/Sprites/textures.png", new Coordinates(32, 0));
+		super("Air", "D:/CESI année 1/Projet/Java/Sprites/textures.png", new Coordinates(32, 0));
 	}
 	
 	@Override

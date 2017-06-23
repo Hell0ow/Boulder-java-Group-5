2cf0a492d67bd38ceb8edbc0033091e61b1a03d4
NicolasV-h <nicolas.vanhove@viacesi.fr>
2017-06-23 16:30:43 +0200

    Update State.java

--- BoulderDash/view/src/main/java/state/State.java
+++ BoulderDash/view/src/main/java/state/State.java
@@ -9,19 +9,33 @@ public abstract class State {
 
 
     private static State currentState = null;
-
+/**
+     * Set state.
+     *
+     * @param state the state
+     */
     public static void setState(State state){
         currentState = state;
     }
-
+ /**
+     * Get state state.
+     *
+     * @return the state
+     */
     public static State getState(){
         return currentState;
     }
 
     //CLASS
-
+/**
+     * Tick.
+     */
     public abstract  void tick();
-
+/**
+     * Render.
+     *
+     * @param g the g
+     */
     public abstract  void render(Graphics g);
 
 }

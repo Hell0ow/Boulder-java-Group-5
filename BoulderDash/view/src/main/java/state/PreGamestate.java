b16701b195ffc802763bf4dacce9b20dc2250152
NicolasV-h <nicolas.vanhove@viacesi.fr>
2017-06-26 08:52:22 +0200

    Update PreGamestate.java

--- BoulderDash/view/src/main/java/state/PreGamestate.java
+++ BoulderDash/view/src/main/java/state/PreGamestate.java
@@ -1,70 +1,105 @@
- package state;
+package BoulderDash.State;
+
+import BoulderDash.Display;
+import BoulderDash.Game;
+import BoulderDash.gfx.Assets;
 
 import javax.swing.*;
-
-import menu.Game;
-import java.util.List;
 import java.awt.*;
-import java.util.ArrayList;
+import java.io.File;
+import java.io.IOException;
 
 /**
  * Created by Nico on 22/06/2017.
  */
 public class PreGamestate extends State{
 
-    private static String mapName;
-    private static String playerName;
-    private static boolean asking;
-    private static String mapList[];
-    
-    /**
-     * Instantiates a new Pre gamestate.
-     */
+
+    private int imageWidth, imageHeight;
+
+    private static int mapChoosen;
+
+    private static String playerNickname;
+    private boolean check = false;
+    private static boolean game = false;
+    String[] array = {"Hell", "test", "Espoir", "Antoine"};
 
     public PreGamestate(){
-        mapName = "";
-        playerName = "";
-        asking = false;
+        imageHeight = 400;
+        imageWidth = 400;
+        mapChoosen = 0;
     }
 
     @Override
     public void tick() {
-        playerName = JOptionPane.showInputDialog("What is your nickname?");
-        mapName = JOptionPane.showInputDialog("Which map will you choose?");
-        asking = true;
+        if(!check) {
+            playerNickname = JOptionPane.showInputDialog("What's your nickname?");
+            check = true;
+        }
+
+        if(game)
+            State.setState(Game.getGamestate());
     }
-    /**
-     *
-     * @param g the g
-     */
 
     @Override
     public void render(Graphics g) {
+        g.drawImage(Assets.background, 0,0, Display.getWidthframe(),Display.getHeightframe(), null);
 
-        State.setState(Game.getGamestate());
-        
+
+        Font fnt0 = null;
+        try {
+            fnt0 = Font.createFont(Font.TRUETYPE_FONT, new File("out/font/slkscr.ttf")).deriveFont(Font.PLAIN, 55);
+        } catch (FontFormatException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        g.setFont(fnt0);
+        g.setColor(Color.red);
+
+        g.drawString("PLAY !", 490,780);
+
+        g.drawString("Choose your map: ", 300,100);
+
+
+        g.drawImage(Assets.right_arrow,800,300, 200,110,null);
+        g.drawImage(Assets.left_arrow, 150, 300,200,100,null);
+
+        g.setColor(Color.green);
+
+        if(mapChoosen == 0){
+            g.drawString(array[0], 490,280);
+            g.drawImage(Assets.map0, 380,300,imageWidth,imageHeight,null);
+        }
+        if(mapChoosen == 1){
+            g.drawString(array[1], 490,280);
+            g.drawImage(Assets.map1, 380,300,imageWidth,imageHeight,null);
+        }
+        if(mapChoosen == 2) {
+            g.drawString(array[2], 490, 280);
+            g.drawImage(Assets.map2, 380,300,imageWidth, imageHeight, null);
+        }
+        if(mapChoosen == 3){
+            g.drawString(array[3], 490,280);
+            g.drawImage(Assets.map3, 380,300,imageWidth, imageHeight, null);
+        }
     }
-    
-    public static String  getMapName() throws InterruptedException{
-    	while(asking == false){
-    		Thread.sleep(1);
-    	}
-    	return mapName;
+
+
+    public static boolean isGame() {
+        return game;
     }
-    
-    public static String  getPlayerName() throws InterruptedException{
-    	while(asking == false){
-    		Thread.sleep(1);
-    	}
-    	return playerName;
+
+    public static void setGame(boolean gamee) {
+        game = gamee;
     }
-    
-    public static void setMapList(String pmapList[]){
-    	int sizeTable = pmapList.length;
-    	mapList = new String[sizeTable];
-    	
-    	for(int i = 0; i < sizeTable; i++){
-    		mapList[i] = pmapList[i];
-    	}
+
+    public static  int getMapChoosen() {
+        return mapChoosen;
+    }
+
+
+    public static void setMapChoosen(int map) {
+        mapChoosen = map;
     }
 }

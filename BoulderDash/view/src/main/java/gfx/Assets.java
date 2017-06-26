a58a98032026b18ac4491b277b71c85ff4104e00
NicolasV-h <nicolas.vanhove@viacesi.fr>
2017-06-26 08:53:52 +0200

    Update Assets.java

--- BoulderDash/view/src/main/java/gfx/Assets.java
+++ BoulderDash/view/src/main/java/gfx/Assets.java
@@ -5,33 +5,27 @@ import java.awt.image.BufferedImage;
 
 /**
  * Created by Nicolas on 21/06/2017.
- * load all the images
  */
 public class Assets {
-/**
-     * The constant background.
-     */
-    public static BufferedImage background,
-    /**
-     * The Title animation.
-     */
-    titleAnimation;
-    /**
-     * The image for Display the fps number and set the music's volume
-     */
-    public static BufferedImage speaker_muted, speaker_low, speaker_high, canSee, cantSee;
+
+    public static BufferedImage background, titleAnimation;
+    public static BufferedImage speaker_muted, speaker_low, speaker_high, canSee, cantSee, left_arrow, right_arrow;
     public static Rectangle playButton, settingsButton;
-    /**
-    * Init. load image
-    */
+    public static BufferedImage map0, map1, map2, map3;
 
     public static void init(){
-        background = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/backgroun.png");
-        titleAnimation = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/Title_Boulder.png");
-        speaker_muted = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/no_volume.png");
-        speaker_low = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/minus_volume.png");
-        speaker_high = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/plus_volume.png");
-        canSee = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/yeux.png");
-        cantSee = ImageLoader.loadImage("D:/CESI année 1/Projet/Java/Sprites/yeux_barre.png");
+        background = ImageLoader.loadImage("out/img/backgroun.png");
+        titleAnimation = ImageLoader.loadImage("out/img/Title_Boulder.png");
+        speaker_muted = ImageLoader.loadImage("out/img/no_volume.png");
+        speaker_low = ImageLoader.loadImage("out/img/minus_volume.png");
+        speaker_high = ImageLoader.loadImage("out/img/plus_volume.png");
+        canSee = ImageLoader.loadImage("out/img/yeux.png");
+        cantSee = ImageLoader.loadImage("out/img/yeux_barre.png");
+        map0 = ImageLoader.loadImage("out/img/map0.PNG");
+        map1 = ImageLoader.loadImage("out/img/map1.PNG");
+        map2 = ImageLoader.loadImage("out/img/map2.PNG");
+        map3 = ImageLoader.loadImage("out/img/map3.PNG");
+        left_arrow =  ImageLoader.loadImage("out/img/fleche_gauche.PNG");
+        right_arrow = ImageLoader.loadImage("out/img/fleche_droit.PNG");
     }
 }
